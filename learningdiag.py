c4c4c30fed877adaed91e9861aec0b0b3bf7e06c
flyer19820218 <flyer19820218@gmail.com>
2026-04-23 14:22:56 +0800

    Update learningdiag.py

--- learningdiag.py
+++ learningdiag.py
@@ -469,7 +469,7 @@ if st.session_state.app_phase == "checkin":
     col1, col2, col3 = st.columns([1, 2.5, 1])
     
     with col2:
-        st.markdown("<h1 style='text-align: center; margin-bottom: 0;'>⚾ 化學大聯盟</h1>", unsafe_allow_html=True)
+        st.markdown("<h1 style='text-align: center; margin-bottom: 0;'>科學大聯盟</h1>", unsafe_allow_html=True)
         st.write("---")
         
         tab1, tab2, tab3 = st.tabs(["🧑‍🎓 一般球員報到", "🛡️ 教練專屬通道", "🌟 801 專屬通道"])
